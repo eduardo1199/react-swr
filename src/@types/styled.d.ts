9d139be2a97b927dd98ad3df7e6b15de967ff48b
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-05-27 22:19:33 -0300

    feat: implementation initial setup project

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -4,5 +4,6 @@ import { defaultTheme } from 'pages/styles/theme/defaultTheme'
 type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends ThemeType {}
 }
