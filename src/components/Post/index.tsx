e9b9a3aa5d15e122a5f6fb6c1b3c037654846160
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-08-20 18:44:28 -0300

    feat: fetch issues

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,14 +1,22 @@
+import { useNavigate } from 'react-router-dom'
 import { CardPost } from './styles'
 
 interface PostProps {
   title: string
   datePosted: string
   about: string
+  url: string
 }
 
-export function Post({ about,datePosted, title}: PostProps) {
+export function Post({ about,datePosted, title, url }: PostProps) {
+  const navigate = useNavigate()
+
+  function handleNavigateToRepo() {
+    navigate(url)
+  }
+
   return (
-    <CardPost>
+    <CardPost onClick={() => handleNavigateToRepo()}>
       <div>
         <h1>
           {title}
